eb0d28e1d0fd12043f5cd5f5043bdafab229aa3c
Mete <126513337+mmetem55@users.noreply.github.com>
2026-04-11 18:30:16 +0300

    upload device tree

---
+++ twrp-11-device-tree/aidata/ADT1061/device.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/aidata/ADT1061
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery.fstab:recovery/root/system/etc/recovery.fstab
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := hdpi
